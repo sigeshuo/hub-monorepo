525efe9dc326069ae59fdf66ef530214a99d4047
sigeshuo <sigeshuo@outlook.com>
2024-06-28 16:20:03 +0800

    fix

--- scripts/publish-image.sh
+++ scripts/publish-image.sh
@@ -13,11 +13,10 @@ HUBBLE_VERSION=$(node -e "console.log(require('./apps/hubble/package.json').vers
 echo "Publishing $HUBBLE_VERSION"
 
 # Enable Buildx
-docker buildx use mybuilder
+docker buildx create --use
 
 docker buildx build -f Dockerfile.hubble \
   --platform "linux/amd64,linux/arm64" \
-  --push \
   -t farcasterxyz/hubble:${HUBBLE_VERSION} \
   -t farcasterxyz/hubble:latest \
   .
