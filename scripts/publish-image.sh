3260bd813fa54284df40abd3ccb8ecb6ab722a9d
sigeshuo <sigeshuo@outlook.com>
2024-06-28 11:43:05 +0800

    docker build

--- scripts/publish-image.sh
+++ scripts/publish-image.sh
@@ -12,7 +12,7 @@ HUBBLE_VERSION=$(node -e "console.log(require('./apps/hubble/package.json').vers
 
 echo "Publishing $HUBBLE_VERSION"
 
-depot build -f Dockerfile.hubble \
+docker build -f Dockerfile.hubble \
   --platform "linux/amd64,linux/arm64" \
   --push \
   -t farcasterxyz/hubble:${HUBBLE_VERSION} \
