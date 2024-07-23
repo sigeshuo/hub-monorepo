ff4074447a5dd7d881a697f1d95e1fa906684dd2
sigeshuo <sigeshuo@outlook.com>
2024-07-23 10:12:37 +0800

    fix

--- scripts/hubble.sh
+++ scripts/hubble.sh
@@ -7,7 +7,7 @@
 # Define the version of this script
 CURRENT_VERSION="5"
 
-REPO="sigeshuo1/hub-monorepo"
+REPO="sigeshuo/hub-monorepo"
 RAWFILE_BASE="https://raw.githubusercontent.com/$REPO"
 LATEST_TAG="@latest"
 
